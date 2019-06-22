9fca902522069108d860f008b4569cc205f38e5a
Lilu <KHussell@gmail.com>
2019-06-22 14:29:23 -0400

    added jquery link to main.handlebars, created markup for landing, login, and plant device pages

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,16 +4,35 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+      res.render("landing", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
 
+  app.get("/", function(req, res) {
+    res.render("landing");
+  });
+
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+
+  app.get("/plantDevices", function(req, res) {
+    res.render("plantDevices");
+  });
+
+  app.get("/plantDevices#addDevice", function(req, res) {
+    $(".modal").modal();
+    res.send("hi");
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
