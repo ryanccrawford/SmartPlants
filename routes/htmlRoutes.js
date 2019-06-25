dfa3c710ae7bf71bcff0516eb786c1f394c4546b
Ben Miller <mail@benkmiller.com>
2019-06-25 18:04:25 -0400

    Formatted graphs and tables

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,6 @@
-var db = require("../models");
+//var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("landing", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
   app.get("/", function(req, res) {
     res.render("landing");
   });
@@ -26,17 +16,6 @@ module.exports = function(app) {
   app.get("/plantDevices#addDevice", function(req, res) {
     $(".modal").modal();
     res.send("hi");
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
   });
 
   // Render plant page - Will add ID later
