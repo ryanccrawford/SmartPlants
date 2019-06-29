3aea974918c9d60f34c9490dea58d7286a8d9e01
RyBo <ryanccrawford@live.com>
2019-06-29 09:43:44 -0400

    Started new readme, cron-task

--- task/cron-task.js
+++ task/cron-task.js
@@ -1,6 +1,10 @@
 const cron = require("node-cron")
 
 //running every 2 minutes
-cron.schedule('*/2 * * * *', () => {
-    
+cron.schedule('*/45 * * * *', () => {
+    function makeHistory() {
+
+        var sql = "INSERT INTO "
+
+    }
 });
