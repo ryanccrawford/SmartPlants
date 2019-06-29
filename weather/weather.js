89a8f2c44f6fce9b3819904ff9104768b076bc2e
RyBo <ryanccrawford@live.com>
2019-06-29 04:56:30 -0400

    wather API

--- weather/weather.js
+++ weather/weather.js
@@ -1,7 +1,7 @@
 const weather = require('weather-js');
 
-var w = function (cb, cityState, zip) {
-    if (cityState == '' && zip == '') {
+var w = function (cb, zip) {
+    if (zip == '') {
         console.log("City and State or Zip code is missing, using defualt location")
 
         weather.find({ search: 'Orlando, FL', degreeType: 'F' }, function (err, result) {
@@ -10,9 +10,7 @@ var w = function (cb, cityState, zip) {
             cb(result)
         });
     } else {
-        var city = cityState.split(" ")[0]
-        var state = cityState.split(" ")[1]
-        weather.find({ search: (city + ', ' + state) , degreeType: 'F' }, function (err, result) {
+        weather.find({ search: zip , degreeType: 'F' }, function (err, result) {
             if (err) console.log(err);
             console.log(result)
             cb(result)
