29a8c3b8f54b315099d67c71df193b3eca5b4fe6
RyBo <ryanccrawford@live.com>
2019-07-01 09:38:21 -0400

    Changed device driver and weather api to work without using an express server just using native functions. Much faster and cleaner. Does not effect the backend code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
 
   app.post("/api/user", function(req, res) {
     db.User.create(req.body)
-      .then(function(dbUser) {
+      .then(function(dbUser) {                                                                                                                                                                      
         res.json(dbUser);
       })
       .catch(function(err) {
